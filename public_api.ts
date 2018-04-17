40b86bb23d1ca9b663d4295453a76bf04eba4858
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-17 17:30:22 +1000

    Support exports for Annotation and AnnotationComponent

--- public_api.ts
+++ public_api.ts
@@ -1,1 +1,3 @@
-export * from './src/app/transcriber/annotator.module'
+export * from './src/app/transcriber/annotator.module';
+export * from './src/app/transcriber/annotator.component';
+export * from './src/app/transcriber/shared/annotation';

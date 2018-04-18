5f24a4831bc52f90589d63607d4f079e42eb226f
David L. Irving (SPanda) <david.l.irving@gmail.com>
2018-04-18 10:42:26 +1000

    Update paths

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,3 @@
-export * from './src/app/transcriber/annotator.module';
-export * from './src/app/transcriber/annotator.component';
+export * from './src/app/transcriber/alveo-transcriber.module';
+export * from './src/app/transcriber/alveo-transcriber/alveo-transcriber.component';
 export * from './src/app/transcriber/shared/annotation';
